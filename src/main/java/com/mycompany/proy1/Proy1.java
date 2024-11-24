50152922d8c3271dfe26050cb9b524f2c84fe1f0
JANETH <JANETH@DESKTOP-2321OEE>
2024-11-24 18:20:17 -0500

    codigo

---
+++ src/main/java/com/mycompany/proy1/Proy1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proy1;
+import java.util.Scanner;
+/**
+ *
+ * @author JANETH
+ */
+public class Proy1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Tablas de multiplicar");
+        System.out.print("Introduce un numero: ");
+        int numero = scanner.nextInt();
+
+        // Generar la tabla de multiplicar desde 0 hasta 12
+        System.out.println("La tabla del numero " + numero + "es:");
+        for (int i = 0; i <= 12; i++) {
+            System.out.println(numero + " * " + i + " = " + (numero * i));
+        }
+
+        scanner.close();
+    }
+}
